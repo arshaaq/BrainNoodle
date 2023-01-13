87e001f30c9551aec204c749a9e593eb3fe493f2
ScribCoding <105490558+ScribCoding@users.noreply.github.com>
2023-01-13 09:00:07 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -154,7 +154,7 @@ let iphoneScroll = gsap.to("#iphone_screen",{
     y:-710,
     
 scrollTrigger:{
-        markers: true,
+        
         trigger:"#functions_container",
         start: "61% center",
         end: "+=899",
